d1ef083a15ab10d0394cdff1d48033424f62a181
juansrz <95307238+juansrz@users.noreply.github.com>
2024-03-05 01:50:40 +0100

    inicializacion del proyecto
    creado las diferentes clases para la página, agregado dependecias,creación del proyecto

---
+++ SpartanWrath/src/main/java/com/spartanwrath/model/pedido.java
@@ -0,0 +1,76 @@
+package com.spartanwrath.model;
+
+import java.util.Date;
+
+public class pedido {
+    private Integer id;
+    private String numero;
+    private Date fechaCreacion;
+    private Date fechaRecibido;
+
+    private double total;
+
+    public pedido() {
+
+    }
+
+    public pedido(Integer id, String numero, Date fechaCreacion, Date fechaRecibido, double total) {
+        super();
+        this.id = id;
+        this.numero = numero;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaRecibido = fechaRecibido;
+        this.total = total;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaRecibido() {
+        return fechaRecibido;
+    }
+
+    public void setFechaRecibido(Date fechaRecibido) {
+        this.fechaRecibido = fechaRecibido;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "pedido{" +
+                "id=" + id +
+                ", numero='" + numero + '\'' +
+                ", fechaCreacion=" + fechaCreacion +
+                ", fechaRecibido=" + fechaRecibido +
+                ", total=" + total +
+                '}';
+    }
+}
---
+++ SpartanWrath/src/main/java/com/spartanwrath/model/usuario.java
@@ -0,0 +1,95 @@
+package com.spartanwrath.model;
+
+public class usuario {
+    private Integer id;
+    private String nombre;
+    private String usename;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+
+    public usuario(){
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsename() {
+        return usename;
+    }
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", usename='" + usename + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
