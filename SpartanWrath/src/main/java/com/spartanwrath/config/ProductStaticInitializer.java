4f7d3ee110911d7878d0a9a3d53e0bfb7def9cc0
juansrz <95307238+juansrz@users.noreply.github.com>
2024-03-11 17:18:12 +0100

    Products initialization
    He eliminado el MarketController y añadido un archivo config con un PostConstruct con los productos.

---
+++ SpartanWrath/src/main/java/com/spartanwrath/config/ProductStaticInitializer.java
@@ -0,0 +1,34 @@
+package com.spartanwrath.config;
+
+import com.spartanwrath.model.Product;
+import com.spartanwrath.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ProductStaticInitializer {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductStaticInitializer(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostConstruct
+    public void initializeStaticProducts() {
+        List<Product> staticProducts = new ArrayList<>();
+        staticProducts.add(new Product("Casco", "Casco de proteccion para sparring", "../../images/casco.jepg", 10.00, 2, ""));
+        staticProducts.add(new Product("Espinilleras", "Espinilleras de proteccion para Kick Boxing/Muai Thai", "../../images/espinilleras.jpg", 12.00, 2, ""));
+        staticProducts.add(new Product("Guantes", "Guantes de boxeo 16 Oz de piel sintetica", "../images/guantes.png", 49.99, 2, ""));
+        staticProducts.add(new Product("Vendas", "Vendas 4.5 metros", "../images/vendas.jpg", 5.99, 2, ""));
+        staticProducts.add(new Product("Bucal", "Bucal de proteccion para sparring", "../images/bucal.jpg", 3.00, 2, ""));
+        staticProducts.add(new Product("Proteina", "Whey Protein facilita el proceso", "../images/proteina.jpg", 24.99, 2, ""));
+
+        productRepository.saveAll(staticProducts);
+    }
+}
--- SpartanWrath/src/main/java/com/spartanwrath/repository/ProductRepository.java
+++ SpartanWrath/src/main/java/com/spartanwrath/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.spartanwrath.repository;
 import com.spartanwrath.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
