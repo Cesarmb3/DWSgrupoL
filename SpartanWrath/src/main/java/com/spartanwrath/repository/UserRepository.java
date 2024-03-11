1d66446211af2d8ce27d18854c65810dabfa5bd8
juansrz <95307238+juansrz@users.noreply.github.com>
2024-03-11 23:55:52 +0100

    Update UserRepository.java

--- SpartanWrath/src/main/java/com/spartanwrath/repository/UserRepository.java
+++ SpartanWrath/src/main/java/com/spartanwrath/repository/UserRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
-    List<User> findByCategory(String category);
+    //List<User> findByCategory(String category);
 }
