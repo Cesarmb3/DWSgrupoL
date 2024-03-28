ca44a003124f2bdcbce0e9c1d5cba942a625e305
Cesarmb3 <146852522+Cesarmb3@users.noreply.github.com>
2024-03-28 21:30:19 +0100

    mas

---
+++ SpartanWrath/src/main/java/com/spartanwrath/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.spartanwrath.repository;
+
+import com.spartanwrath.model.Product;
+import com.spartanwrath.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
+    //List<User> findByCategory(String category);
+}
