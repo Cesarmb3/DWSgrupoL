b607c7e1989b61e995e6c52fda52c4dd88315116
juansrz <95307238+juansrz@users.noreply.github.com>
2024-04-02 16:29:23 +0200

    Update UserRestController.java
    Metodo Get by username y Post para crear nuevo usuario

--- SpartanWrath/src/main/java/com/spartanwrath/restController/UserRestController.java
+++ SpartanWrath/src/main/java/com/spartanwrath/restController/UserRestController.java
@@ -1,14 +1,15 @@
 package com.spartanwrath.restController;
 
 
+import com.spartanwrath.exceptions.InvalidUser;
 import com.spartanwrath.exceptions.NoUsers;
+import com.spartanwrath.exceptions.UserAlreadyRegister;
+import com.spartanwrath.exceptions.UserNotFound;
 import com.spartanwrath.model.User;
 import com.spartanwrath.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +29,27 @@ public class UserRestController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username){
+        try {
+            User user = userServ.getUserbyUsername(username);
+            return ResponseEntity.ok().body(user);
+        } catch (UserNotFound userNotFound){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<User> newUser(@RequestBody User user){
+        try {
+            userServ.add(user);
+            return ResponseEntity.ok().body(user);
+        } catch (UserAlreadyRegister | InvalidUser e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
 
 // Arreglar lista de productos a usuario y viceversa
